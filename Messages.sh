7f7f5a9029bf9cbd0a1cd2215d9370dfb35f1a0a
Parker Coates <coates@kde.org>
2008-10-07 00:21:06 +0000

    Added .rc, .ui, and .kcfg files to Messages.sh.
    svn path=/trunk/kdereview/killbots/; revision=868742

--- Messages.sh
+++ Messages.sh
@@ -1,2 +1,8 @@
-#!/bin/sh
-$XGETTEXT *.cpp -o $podir/killbots.pot
+#!bin/sh
+# invoke the extractrc script on all .ui, .rc, and .kcfg files in the sources
+# the results are stored in a pseudo .cpp file to be picked up by xgettext.
+$EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
+
+# call xgettext on all source files. If your sources have other filename
+# extensions besides .cc, .cpp, and .h, just add them in the find call.
+$XGETTEXT `find . -o -name \*.cpp -o -name \*.h` -o $podir/killbots.pot
