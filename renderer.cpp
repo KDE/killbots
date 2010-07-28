17695b8841f0dc038ce37ff9945b06e3a30d5bd9
Parker Coates <coates@kde.org>
2010-07-28 16:26:20 +0000

    Make Renderer's custom features load on demand.
    Calculating this data at the end of setTheme proved to be too late, as
    by that time the themeChanged signal has been emitted. Loading them on
    demand just sidesteps that whole issue.
    
    svn path=/trunk/KDE/kdegames/killbots/; revision=1156144

--- renderer.cpp
+++ renderer.cpp
@@ -38,7 +38,8 @@ Killbots::Renderer * Killbots::Renderer::self()
 
 
 Killbots::Renderer::Renderer()
-  : KGameRenderer( Settings::defaultThemeValue() )
+  : KGameRenderer( Settings::defaultThemeValue() ),
+    m_aspectRatio( 0 )
 {
 	setTheme( Settings::theme() );
 }
@@ -46,39 +47,46 @@ Killbots::Renderer::Renderer()
 
 QCursor Killbots::Renderer::cursorFromAction( int direction )
 {
+	if ( m_cursors.isEmpty() )
+	{
+		for ( int i = 0; i <= 8; ++i )
+		{
+			const QPixmap pixmap = spritePixmap( QString("cursor%1").arg( i ), QSize( 42, 42 ) );
+			if ( !pixmap.isNull() )
+				m_cursors.insert( i, QCursor( pixmap ) );
+		}
+	}
+
 	return m_cursors.value( direction, Qt::ArrowCursor );
 }
 
 
 QColor Killbots::Renderer::textColor()
 {
+	if ( !m_textColor.isValid() )
+		m_textColor = spritePixmap( "textcolor", QSize( 3, 3 ) ).toImage().pixel( 1, 1 );
+
 	return m_textColor;
 }
 
 
 qreal Killbots::Renderer::aspectRatio()
 {
+	if ( !m_aspectRatio )
+	{
+		const QRectF tileRect = boundsOnSprite("cell");
+		m_aspectRatio = qBound<qreal>( 0.3333, tileRect.width() / tileRect.height(), 3.0 );
+	}
+
 	return m_aspectRatio;
 }
 
 
 void Killbots::Renderer::setTheme( const QString & theme )
 {
+	m_aspectRatio = 0;
+	m_textColor = QColor();
+	m_cursors.clear();
+
 	KGameRenderer::setTheme( theme );
-
-	// Get the theme's aspect ratio from the .desktop file.
-	const QRectF tileRect = boundsOnSprite("cell");
-	m_aspectRatio = tileRect.width() / tileRect.height();
-	m_aspectRatio = qBound<qreal>( 0.3333, m_aspectRatio, 3.0 );
-
-	// Get the theme's text color from the "textcolor" SVG element.
-	m_textColor = spritePixmap( "textcolor", QSize( 3, 3 ) ).toImage().pixel( 1, 1 );
-
-	// Generate cursors.
-	for ( int i = 0; i <= 8; ++i )
-	{
-		const QPixmap pixmap = spritePixmap( QString("cursor%1").arg( i ), QSize( 42, 42 ) );
-		if ( !pixmap.isNull() )
-			m_cursors.insert( i, QCursor( pixmap ) );
-	}
 }
