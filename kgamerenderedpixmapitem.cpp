cbda56b5fd5adfdf57682a4ee07eaffcbbb23fd8
Parker Coates <coates@kde.org>
2010-07-28 16:26:24 +0000

    Add a KGameRenderedPixmapItem class as a simpler alternative to KGRI.
    While it has some fancy features, KGameRenderedItem's complexity can
    also prove a hindrance. It doesn't provide access to the full QGPI
    API. It adds the wait of QObject to every item (which is probably not a
    big deal, but it bugs me.) One cannot override the implementation of
    paint() because of the secret child item. Etc.
    
    For the simple needs of Killbots all that is really needed is a
    QGPI that can automatically fetch its pixmap from the KGR. This simple,
    simple, simple class provides that.
    
    svn path=/trunk/KDE/kdegames/killbots/; revision=1156146

---
+++ kgamerenderedpixmapitem.cpp
@@ -0,0 +1,39 @@
+/*
+ *  Copyright 2010  Parker Coates <parker.coates@kdemail.net>
+ *
+ *  This file is part of Killbots.
+ *
+ *  Killbots is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Killbots is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Killbots. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "kgamerenderedpixmapitem.h"
+
+
+KGameRenderedPixmapItem::KGameRenderedPixmapItem( KGameRenderer * renderer, const QString & spriteKey, QGraphicsItem * parent )
+  : QGraphicsPixmapItem( parent ),
+    KGameRendererClient( renderer, spriteKey )
+{
+}
+
+
+KGameRenderedPixmapItem::~KGameRenderedPixmapItem()
+{
+}
+
+
+void KGameRenderedPixmapItem::receivePixmap( const QPixmap & pixmap )
+{
+	setPixmap( pixmap );
+}
+
