3d7719adc7f3c8a19199d901cb1fb9c5867e0eb4
Friedrich W. H. Kossebau <kossebau@kde.org>
2020-12-31 12:54:09 +0100

    Convert license statements to SPDX expressions
    Also add/move license files as required by REUSE specification
    
    GIT_SILENT

--- src/ruleset.h
+++ src/ruleset.h
@@ -1,21 +1,10 @@
 /*
- *  Copyright 2007-2009  Parker Coates <coates@kde.org>
- *
- *  This file is part of Killbots.
- *
- *  Killbots is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  Killbots is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with Killbots. If not, see <http://www.gnu.org/licenses/>.
- */
+    This file is part of Killbots.
+
+    SPDX-FileCopyrightText: 2007-2009 Parker Coates <coates@kde.org>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
 
 #ifndef KILLBOTS_RULESET_H
 #define KILLBOTS_RULESET_H
